f828dfe7afa3ec8319a995bd8b6468472f571fcf
khushbu <khushbu@hivepro.cloud>
2021-10-08 12:04:00 +0530

    removed httpcontext from exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Button from './components/Button/Button';
-import httpService, { HttpContext } from './provider/http/http';
+import httpService from './provider/http/http';
 import { HttpConstants, HttpMethodTypes, HttpResponseTypes, URL } from './provider/http/server.constants';
 
 export {
-    Button, httpService, HttpContext, HttpMethodTypes, URL,
+    Button, httpService, HttpMethodTypes, URL,
     HttpConstants,
     HttpResponseTypes
 };
