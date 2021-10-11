584a4598681c12e774e0320f79a899285b3ed8b1
khushbu <khushbu@hivepro.cloud>
2021-10-11 15:08:33 +0530

    vresion upated

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Button from './components/Button/Button';
-import httpService from './provider/http/http';
-import { HttpConstants, HttpMethodTypes, HttpResponseTypes, URL } from './provider/http/server.constants';
-export { Button, httpService, HttpMethodTypes, URL, HttpConstants, HttpResponseTypes };
